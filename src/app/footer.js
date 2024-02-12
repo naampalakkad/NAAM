f781fcc9e8e8bc8dae3beac570831b44ad8fcaf0
Anjitha5559 <84130915+Anjitha5559@users.noreply.github.com>
2024-02-12 14:46:16 +0530

    Anjitha - 12/02

--- src/app/footer.js
+++ src/app/footer.js
@@ -1,27 +1,10 @@
-'use client';
-
-import React from 'react';
-
-const styles = {
-  footer: {
-    backgroundColor: '#f5f5f5',
-    padding: '48px',
-    textAlign: 'center',
-  },
-  link: {
-    color: 'inherit',
-    textDecoration: 'none',
-  },
-};
-
-function Footer() {
+import React from "react";
+import "./footer.css";
+const Footer = () => {
   return (
-    <footer style={styles.footer}>
-      <div style={{ maxWidth: '64rem', margin: '0 auto' }}>
-            footer
-      </div>
-    </footer>
-  );
-}
-
+    <footer>
+<div className="footer">This is footer</div>
+</footer>
+    );
+};
 export default Footer;
--- src/app/header.js
+++ src/app/header.js
@@ -1,37 +1,18 @@
-'use client';
-
-import React from 'react';
-import { Box, Button, Flex, Heading , ButtonGroup} from '@chakra-ui/react';
-
-function Header() {
+import React from "react";
+import "./header.css";
+const Header = () => {
   return (
-    <Box as="header" bg="#3f51b5" color="white" p={2}>
-      <Flex align="center" justify="space-between">
-        <Button ml={4} mr={4} variant="ghost" aria-label="menu">
-          ☰
-        </Button>
-        <Heading flexGrow={1} textAlign="center">
-          NAAM
-        </Heading>
-        <Box direction='row' spacing={4} align='center'>
-          <Button colorScheme='teal' variant='solid'>
-            Button
-          </Button>
-          <Button colorScheme='teal' variant='outline'>
-            Button
-          </Button>
-          <Button colorScheme='teal' variant='ghost'>
-            Button
-          </Button>
-          <Button colorScheme='teal' variant='link'>
-            Button
-          </Button>
-        </Box>
-      </Flex>
-    </Box>
-
-  );
-}
-
-
+    <header className="header">
+      <div className="main_logo">
+        <img src="" alt="NAAM Logo"/>
+      </div>
+      <div className="heading">
+        NAAM
+      </div>
+      <div className="menu">
+        this is menus
+      </div>
+    </header>
+    );
+};
 export default Header;
