b09e1c77a9bf76694a5ed10d5b5c2bde6873e82f
Anjitha5559 <84130915+Anjitha5559@users.noreply.github.com>
2024-02-12 16:21:23 +0530

    AJ 12/2

--- src/app/header.js
+++ src/app/header.js
@@ -1,15 +1,29 @@
 import React from "react";
+import Image from 'next/image'
+import "./header.css";
 const Header = () => {
   return (
     <header className="header">
       <div className="main_logo">
-        logo
+      <Image src={`/assets/logo.png`} alt={"Main logo"} width="100" height="64" />
       </div>
       <div className="heading">
+        <div className="main_head">
         NAAM
+        </div>
+        <div className="sub_head">
+        Navodaya Alumni Association, Malampuzha
+        </div>
+      
       </div>
-      <div className="menu">
-        this is menus
+      <div className="main_menu">
+        <div className="gallery">Gallery</div>
+        <div className="Alum">Alum</div>
+        <div className="News">News</div>
+        <div className="Calender">Calender</div>
+        <div className="About">About Us</div>
+        <div className="Register">Register</div>
+
       </div>
     </header>
     );
