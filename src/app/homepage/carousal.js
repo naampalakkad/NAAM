d535e9ac2972824516ec54711f0b4ffc2554e77d
Sreejith KS <sreejithksgupta2255@gmail.com>
2024-02-15 13:06:15 +0530

    SK: Carousal_img_animation

--- src/app/homepage/carousal.js
+++ src/app/homepage/carousal.js
@@ -1,60 +1,54 @@
 'use client';
 import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
 
-let carousalimgs = {
-    img1: "./assets/carousalimgs/1.jpg",
-    img2: "./assets/carousalimgs/2.jpg",
-    img3: "./assets/carousalimgs/3.jpg",
-    img4: "./assets/carousalimgs/4.jpg",
-    img5: "./assets/carousalimgs/5.jpg",
-    img6: "./assets/carousalimgs/6.jpg",
-};
-
+let carousalimgs = [
+     "./assets/carousalimgs/1.webp",
+     "./assets/carousalimgs/2.webp",
+     "./assets/carousalimgs/3.webp",
+     "./assets/carousalimgs/4.webp",
+     "./assets/carousalimgs/5.webp",
+     "./assets/carousalimgs/6.webp",
+];
 
 let styles = {
-   carousalbox: {
-       width: "100%",
-       height: "100%",
-       overflow: "hidden",
-       position: "relative",
-       backgroundColor: "black"
-   },
+    carousalbox: {
+        position: "relative",
+        width: "100%",
+        height: "65vh",
+        overflow: "hidden",
+        backgroundColor: "black",
+    },
     carousalimg: {
-        width: "100vw",
-        height: "60vh",
-        display: "flex",
-        transition: "transform 300ms ease-in-out",
-        opacity: "0.5",
+        width: "100%",
+        height: "100%",
+        objectFit: "cover",
+        opacity: "0.3",
     },
     herotext: {
         position: "absolute",
         top: "50%",
-        left: "10%",
-        color: "yellow",
-        fontSize: "100px",
+        left: "20%",
+        color: "white",
+        fontSize: "5rem",
         fontWeight: "bold",
-        position: "absolute",
-        textAlign: "left",
     },
 };
-
-
-
 
 export default function Carousal(){
   const [currentImg, setCurrentImg] = useState(carousalimgs[0]);
   let currentIndex = 0;
   useEffect(() => {
-    setInterval(() => {
-       currentIndex = (currentIndex + 1) % carousalimgs.length;
-        return carousalimgs[currentIndex];
-    }, 5000);
-
-  }, [carousalimgs]);
+     const interval = setInterval(() => {
+         currentIndex = (currentIndex + 1) % carousalimgs.length;
+         setCurrentImg(carousalimgs[currentIndex]);
+     }, 5000);
+     return () => clearInterval(interval);
+  }, []);
   return (
-     <div style={styles.carousalbox}>
-          <img src={currentImg}  style={styles.carousalimg} />
-          <div style={styles.herotext}>NAAAM: The Navodayan Family</div>
-      </div>
+      <div style={styles.carousalbox}>
+             <motion.img src={currentImg}  key={currentImg}  initial={{ opacity: 0, y: -100  }} animate={{ opacity: 0.5, y:0 }} transition={{ duration: 1 }}  style={styles.carousalimg}   />
+             <div style={styles.herotext}>NAAAM: The Navodayan Family</div>
+        </div>
   );
 }
