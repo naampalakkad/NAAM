aedfcf6e27a3d0accbd60c87d824fc239b641b64
NIRANJANA-03 <niranjanasunilkumar2003@gmail.com>
2024-02-24 10:21:34 +0400

    Your commit message here

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,59 +1,86 @@
-// This is the component that will be rendered when the user navigates to /news
-// It should render a list of news articles/updates/blogs etc. 
+'use client'
 import React from 'react';
-import './page.css'
-let posts = [
+import { Box, Heading } from "@chakra-ui/react";
+
+// Your posts data
+const posts = [
     {
-        title: "Post 1",
-        content: "This is the first post",
-        type: "news",
+        title: "gettogether@dubai",
+        type: "newS",
         date: "14/02/2024",
         author: "Unnimaya T",
         edited: "15/02/2024",
-        url : "https://naam.com/posts/how-to-learn-reactjs-in-2024"
+        url: "https://naam.com/posts/how-to-learn-reactjs-in-2024"
     },
     {
-        title: "Post 2",
-        content: "This is the second post",
-        type: "news",
+        title: "JOB@ABC",
+        type: "JOB",
         date: "15/02/2024",
         author: "Sreejith KS",
         edited: "16/02/2024",
-        url : "https://naam.com/posts/how-to-learn-reactjs-in-2025"
+        url: "https://naam.com/posts/how-to-learn-reactjs-in-2025"
     },
     {
-        title: "Post 3",
-        content: "This is the third post",
-        type: "news",
+        title: "MEET@25",
+        type: "EVENT",
         date: "16/02/2024",
         author: "Anjitha J",
         edited: "17/02/2024",
-        url : "https://naam.com/posts/how-to-learn-reactjs-in-2026"
+        url: "https://naam.com/posts/how-to-learn-reactjs-in-2026"
     },
-    
-  
 ];
 
-export default function Posts() {
+const Blog = () => {
+    const [filter, setFilter] = React.useState("all"); // "all" is the default option
+
+    const filteredPosts = filter === "all" ? posts : posts.filter(post => post.type === filter);
+
     return (
-        <div className='background'>
-            <div style={{display:"flex",flexDirection:"row",padding:"10px",width:"97%"}}>
-            <h1 className='news'>News</h1>
-            <button className='button'><a className='readmore' href>Add </a></button> 
-            </div>
-            
-            <ul >
-            {posts.map((post, index) => (
-                <li key={index} className='bigbox'>
-                <h2  className='title'>{post.title}</h2>
-                <p className='date'>{post.date}</p>
-                <p className='author'>{post.author}</p> 
-                <p className='content'>{post.content}</p>
-                
-                <button className='button'><a className='readmore' href={post.url}>Read more</a></button> 
-                </li>
-            ))}
-            </ul>
-        </div>
+        <Box>
+            {/* Blog section */}
+            <Box display="flex" flexDirection="column" alignItems="center" mt="50px" px={['10px', '150px']} mb="50px">
+                <Heading mb="30px">Blog</Heading>
+
+                {/* Filter dropdown */}
+                <Box
+                    mb="20px"
+                    width={['100%', 'auto']}
+                >
+                    <select onChange={(e) => setFilter(e.target.value)} style={{ width: '200px', padding: '10px' }}>
+                        <option value="all">All</option>
+                        <option value="EVENT">Events</option>
+                        <option value="JOB">Job Offers</option>
+                        <option value="newS">Announcements</option>
+                        <option value="BLOG">Blog</option>
+                    </select>
+                </Box>
+
+                {/* Render filtered blog posts horizontally */}
+                <Box
+                    display="flex"
+                    flexDirection={['column', 'row']}
+                    alignItems={['center', 'flex-start']}
+                    justifyContent="space-between"
+                    flexWrap={['wrap', 'nowrap']}
+                    width="100%"
+                >
+                    {/* Render blog posts */}
+                    {filteredPosts.map((post, index) => (
+                        <Box key={index} width={['100%', '30%']} mb={['20px', '0']} borderRadius="8px" overflow="hidden" boxShadow="md" bg="#161a30" color="white">
+                            <Box p="20px" borderBottom="1px solid" backgroundColor="#161a30">
+                                <Heading fontSize={['lg', 'xl']} mb="2">{post.title}</Heading>
+                               
+                                <p>Author: {post.author}</p>
+                                <p>Date: {post.date}</p>
+                                <p>Edited: {post.edited}</p>
+                                <a href={post.url} target="_blank" rel="noopener noreferrer">Read more</a>
+                            </Box>
+                        </Box>
+                    ))}
+                </Box>
+            </Box>
+        </Box>
     );
-    }
+};
+
+export default Blog;
