019f2a460ea9b05cb00c6d53a7d6d045d360b920
Sreejith KS <sreejithksgupta2255@gmail.com>
2024-02-27 21:52:14 +0530

    Base function for saving/updating user details

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,50 +1,51 @@
 "use client"
 import { React, useEffect, useState } from "react";
-import { auth } from "@/lib/firebase";
+import { auth ,db} from "@/lib/firebase";
 import './login.css'
-import { Box, Heading, Image, Center, Stack, Flex, Button, SimpleGrid, Input, InputGroup, InputLeftElement, InputRightElement, CheckIcon } from "@chakra-ui/react";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "firebase/auth";
+import { getDatabase, ref, set } from "firebase/database";
+import { Box, Heading, Image, Button, Input, Card } from "@chakra-ui/react";
+import { GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "firebase/auth";
 
 
 export default function signin() {
     const [user, setUser] = useState(null);
     const [buttonText, setButtonText] = useState('Sign in with Google');
 
-    let personaldetails= [
+    let personaldetails = [
         {
             prop: "Name",
             default: "Enter your name",
-            type : "text"
+            type: "text"
         },
         {
             prop: "Email",
             default: "Enter your email",
-            type : "email"
+            type: "email"
         },
         {
             prop: "batch",
-            default: "Enter your batch",
-            type : "Number"
+            default: "Enter your batch,  eg: 25",
+            type: "Number"
         },
         {
             prop: "number",
             default: "Enter your Mobile number",
-            type : "Number"
+            type: "Number"
         },
         {
             prop: "Facebook Profile",
             default: "Enter your Facebook Profile",
-            type : "text"
+            type: "text"
         },
         {
             prop: "occupation",
             default: "Enter your occupation",
-            type : "text"
+            type: "text"
         },
         {
             prop: "JNV Roll No",
             default: "Enter your JNV Roll No",
-            type : "Number"
+            type: "Number"
         },
 
     ]
@@ -91,31 +92,80 @@ export default function signin() {
             });
     };
 
+    function updatefirebaseuserdata() {
+        console.log("updating user data");
+        let name = document.querySelector(".detaillist:nth-child(1) input").value;
+        let email = document.querySelector(".detaillist:nth-child(2) input").value;
+        let batch = document.querySelector(".detaillist:nth-child(3) input").value;
+        let number = document.querySelector(".detaillist:nth-child(4) input").value;
+        let facebook = document.querySelector(".detaillist:nth-child(5) input").value;
+        let occupation = document.querySelector(".detaillist:nth-child(6) input").value;
+        let rollno = document.querySelector(".detaillist:nth-child(7) input").value;
+
+        let userdetails = {
+            name: name,
+            email: email,
+            batch: batch,
+            number: number,
+            facebook: facebook,
+            occupation: occupation,
+            rollno: rollno
+        }
+        console.log(userdetails);
+
+        savetofirebase(userdetails);
+    }
+
+    function savetofirebase(userDetails) {
+        console.log("Saving to Firebase");
+        if (user) {
+            console.log("User exists");
+            let userRef = ref(db, 'users/' + user.uid);
+            set(userRef, userDetails)
+                .then(() => {
+                    console.log("Document successfully written!");
+                })
+                .catch((error) => {
+                    console.error("Error writing document: ", error);
+                });
+        }
+    }
+
     return (
-        <div className="cardcontainer">
-            
-            <Box className="profilebox">
-            <Heading>Profile</Heading>
-            
-                {user ? (
-                    <div>
-                        <Image src={user.photoURL} alt={user.displayName} />
+        user ? (
+            <div className="cardcontainer">
+                <Card className="profilebox">
+                        <Image src={user.photoURL} className="profileimage" alt={user.displayName} />
                         <p>{user.displayName}</p>
                         <p>{user.email}</p>
                         <Button onClick={user ? handleSignOut : handleSignIn}>Sign out</Button>
-                    </div>
-                ):
-                <Button onClick={user ? handleSignOut : handleSignIn}>Sign In</Button>
-                }
-
-            </Box>
-            <Box spacing={4} className="infobox">
-                {personaldetails.map((detail, index) => (
-                    <div className="detailitem" key={index}>
-                        <Input className="detailitem" variant={"filled"} type={detail.type} placeholder={detail.default} />
-                    </div>
-                ))}
-            </Box>
-        </div>
+                </Card>
+                <Box spacing={4} className="infobox">
+                    {personaldetails.map((detail, index) => (
+                        <div className="detaillist" key={index}>
+                            <label>{detail.prop}</label>
+                            <Input className="detailitem" variant={"filled"} type={detail.type} placeholder={detail.default} />
+                        </div>
+                    ))}
+                    <Button onClick={updatefirebaseuserdata}>Save</Button>
+                </Box>
+            </div>
+        ) :
+            (
+                <div className="cardcontainer">
+                    <Card className="profilebox">
+                        <Heading>Sign In to NAAM website</Heading>
+                        <Button onClick={user ? handleSignOut : handleSignIn}>{buttonText}</Button>
+                    </Card>
+                    <Card className="infobox">
+                        <Heading>Sign In to NAAM website</Heading>
+                        <p>
+                            Sign in with your google account to set your details so that other users can find you, or to view other's details.
+                            We will not share your details with anyone else, and we will not use your details for any other purpose.
+                            Google may collect your details as per their privacy policy.
+                        </p>
+                    </Card>
+                </div>
+            )
     );
 }
--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -9,11 +9,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import {getAuth,  googleAuthProvider, signInWithPopup} from "firebase/auth";
 import { getStorage } from "firebase/storage";
+import {getDatabase} from "firebase/database"
 const firebaseConfig = {
   apiKey: "AIzaSyC97WJwdO5012NyYyRMfjZYUehk6kTZTQI",
   authDomain: "naam-demo.firebaseapp.com",
   projectId: "naam-demo",
   storageBucket: "naam-demo.appspot.com",
+  databaseURL:"https://naam-demo-default-rtdb.asia-southeast1.firebasedatabase.app",
   messagingSenderId: "608229011102",
   appId: "1:608229011102:web:a62c520e1e527f4c3b558f",
   measurementId: "G-NPX2H6Q43C"
@@ -27,3 +29,5 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const storage = getStorage(app);
+// db = realtime database
+export const db = getDatabase(app);
