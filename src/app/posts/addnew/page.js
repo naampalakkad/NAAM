9eed9e8df0d5a04ecdb9fb09f4d2f2c67df716c7
suneeb <ssuneebvishnu@gmail.com>
2024-03-02 00:30:36 +0530

    upload button added

--- src/app/posts/addnew/page.js
+++ src/app/posts/addnew/page.js
@@ -72,7 +72,7 @@ function Page() {
 
           <FormControl>
             <FormLabel>Thumbnail photo</FormLabel>
-            
+               <input type='file'></input>
             <FormHelperText></FormHelperText>
           </FormControl>
 
