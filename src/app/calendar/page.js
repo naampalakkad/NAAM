23e7f1883ed21d9b19e47d65a630e1c37e671ee4
unnimayat <unnimayat01@gmail.com>
2024-03-07 23:26:37 -1200

    calendar

---
+++ src/app/calendar/page.js
@@ -0,0 +1,12 @@
+"use client"
+import { React, useEffect, useState } from "react";
+import { auth ,signInoutWithGoogle, savedatatodb} from "@/lib/firebase";
+import './calendar.css';
+import { Box, Heading, Image, Button, Input, Card } from "@chakra-ui/react";
+import { onAuthStateChanged } from "firebase/auth";
+
+export default function calendar() {
+    return (
+        <div>gjS</div>
+    )
+}
