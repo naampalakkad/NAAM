c1f2f08c192aa139773198c2cd609bc93255d438
NIRANJANA-03 <158493918+NIRANJANA-03@users.noreply.github.com>
2024-03-10 12:22:08 +0400

    FILTERING CHANGES

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,30 +1,24 @@
 'use client'
-import { useState, React, useEffect } from 'react';
-import { Box, Heading, Button } from "@chakra-ui/react";
+import { useState, useEffect } from 'react';
+import { Box, Heading } from "@chakra-ui/react";
 import { getpostsfromdb } from "@/lib/firebase";
-import {BlogPost} from './blogpage'
-
-
+import { BlogPost } from './blogpage';
 
 const Blog = () => {
     const [posts, setPosts] = useState([]);
-    // const [filter, setFilter] = useState("all"); // "all" is the default option
-    // const filteredPosts = filter === "all" ? posts : posts.filter(post => post.type === filter);
+    const [filter, setFilter] = useState("all"); // "all" is the default option
 
     useEffect(() => {
         const fetchPosts = async () => {
             const postsFromDb = await getpostsfromdb();
             console.log(postsFromDb);
             setPosts(Object.entries(postsFromDb));
-            
         };
 
         fetchPosts();
     }, []);
 
-
-
-
+    const filteredPosts = filter === "all" ? posts : posts.filter(post => post[0].type === filter);
     return (
         <Box>
             <Box
@@ -37,16 +31,17 @@ const Blog = () => {
             >
                 <Heading mb="30px">NEWS AND UPDATES</Heading>
                 <Box mb="20px" width={['100%', 'auto']}>
-                    <select
-                        onChange={(e) => setFilter(e.target.value)}
-                        style={{ width: '200px', padding: '10px' }}
-                    >
-                        <option value="all">All</option>
-                        <option value="EVENT">Events</option>
-                        <option value="JOB">Job Offers</option>
-                        <option value="anoun">Announcements</option>
-                        <option value="BLOG">Blog</option>
-                    </select>
+                <select
+                    onChange={(e) => setFilter(e.target.value)}
+                    style={{ width: '200px', padding: '10px' }}
+                >
+                    <option value="all">All</option>
+                    <option value="EVENT">Events</option>
+                    <option value="JOB">Job Offers</option>
+                    <option value="anoun">Announcements</option>
+                    <option value="BLOG">Blog</option>
+                </select>
+
                 </Box>
                 <Box
                     display="grid"
@@ -54,10 +49,9 @@ const Blog = () => {
                     gap={['20px', '20px']}
                     width="100%"
                 >
-              
-                    {posts.map((post, index) => (
+                    {filteredPosts.map((post, index) => (
                         <BlogPost key={index} post={post} />
-                    ))} 
+                    ))}
                 </Box>
             </Box>
         </Box>
