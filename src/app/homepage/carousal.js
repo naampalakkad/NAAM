ae2db655b66c10dc392832075c17a55c52ee8596
Anjitha5559 <84130915+Anjitha5559@users.noreply.github.com>
2024-03-20 21:41:56 +0530

    Anjitha

--- src/app/homepage/carousal.js
+++ src/app/homepage/carousal.js
@@ -28,14 +28,14 @@ export default function Carousal() {
   if (!currentImg) {
     <div className='carousalbox' >
       {/* <motion.img className='carousalimg' src={currentImg}  key={currentImg}  initial={{ opacity: 0, y: -100  }} animate={{ opacity: 0.5, y:0 }} transition={{ duration: 1 }}   /> */}
-      <div className='heroText'>NAAAM: The Navodayan Family</div>
+      <div className='heroText'>NAAM: The Navodayan Family</div>
     </div>
   }
 
   return (
     <div className='carousalbox' >
       <motion.img className='carousalimg' src={currentImg} key={currentImg} initial={{ opacity: 0, y: -100 }} animate={{ opacity: 0.5, y: 0 }} transition={{ duration: 1 }} />
-      <div className='heroText'>NAAAM: The Navodayan Family</div>
+      <div className='heroText'>NAAM: The Navodayan Family</div>
     </div>
   );
 }
