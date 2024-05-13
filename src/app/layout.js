b9d06d8a2fb192e0db0dcfa77658e0e3f710a4c6
sreejith <sreejithksgupta2255@gmail.com>
2024-05-13 14:55:08 +0530

    documenet cleanup and adding header

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import Header from "./components/header";
 import Footer from "./components/footer";
 import { ChakraProvider } from '@chakra-ui/react';
 import "./globals.css"
+import Dochead from "@/lib/docheader"
 
 
 export default function RootLayout({ children }) {
