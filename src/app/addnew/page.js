e5d50984076656c418579d3cbf635e92dfaa42f0
NIRANJANA-03 <niranjanasunilkumar2003@gmail.com>
2024-07-11 15:14:24 +0530

    The Testimonial and filter

--- src/app/addnew/page.js
+++ src/app/addnew/page.js
@@ -4,7 +4,7 @@ import { Input, Button, Select, FormControl, FormLabel, FormHelperText } from '@
 import { ChevronDownIcon } from '@chakra-ui/icons';
 import './page.css';
 import dynamic from 'next/dynamic';
-import { saveposttodb, auth } from '@/lib/firebase';
+import { saveposttodb, auth, savetesttodb } from '@/lib/firebase';
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { useRouter } from 'next/navigation';
 
@@ -12,6 +12,7 @@ if (typeof window !== 'undefined') {
   var ReactQuill = require('react-quill');
   require('react-quill/dist/quill.snow.css');
 }
+
 let ImageCompressor = null
 if (typeof window !== "undefined") {
   import('image-compressor.js')
@@ -23,37 +24,35 @@ if (typeof window !== "undefined") {
     });
 }
 
-// Function to handle saving testimonial data to Firebase
-const saveTestimonialToDb = async (formData, testimonialContent) => {
+const saveTestimonialToDb = async (testimonialContent) => {
   try {
     let testimonialData = {
       authorName: auth.currentUser.displayName,
       content: testimonialContent,
       time: new Date().getTime(),
+
     };
 
-    // Save testimonial data to 'teste' collection in Firebase
-    await saveposttodb('teste', testimonialData);
-
-    // Optionally, you can redirect or show a success message
+    await savetesttodb(testimonialData);
     window.alert("Testimonial saved successfully!");
   } catch (error) {
+    window.alert('Error saving testimonial.');
     console.error('Error saving testimonial:', error);
-    // Handle error as needed
   }
 };
+
 
 function Page() {
   const [value, setValue] = useState('');
   const [formData, setFormData] = useState({
     title: '',
     thumbnail: '',
-    type: '',
+    
   });
 
   const quillRef = useRef();
   const fileInputRef = useRef();
-  const router = useRouter();
+  const router= useRouter();
   const modules = {
     toolbar: [
       [{ header: [1, 2, 3, 4, 5, 6, false] }],
@@ -67,7 +66,8 @@ function Page() {
       matchVisual: false,
     },
   };
-
+   // Thumbnail handle
+  const [isfile, setFile] = useState(null); 
   const onChangeHandler = (e) => {
     setFormData((prevFormData) => ({
       ...prevFormData,
@@ -90,12 +90,11 @@ function Page() {
     }
   };
 
+  // Thumbnail handle
+  
   const handleImageAsFile = (e) => {
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      thumbnail: e.target.files[0],
-    }));
-  };
+    setFile(e.target.files[0]);
+  }
 
   const handleSubmit = async (event) => {
     try {
@@ -104,60 +103,52 @@ function Page() {
         window.alert("Please select the type of post.");
         return;
       }
-      if (formData.type === 'test') {
-        // Handle testimonial data
-        const testimonialContent = value.trim();
-        if (testimonialContent.split(/\s+/).length > 70) {
-          window.alert("Testimonial content should not exceed 70 words.");
-          return;
-        }
-        if (!testimonialContent) {
-          window.alert("Please write your testimonial.");
-          return;
-        }
-        await saveTestimonialToDb(formData, testimonialContent);
-      } else {
-        // Handle regular post data
-        if (!fileInputRef.current || !fileInputRef.current.files[0]) {
-          window.alert("Please select a thumbnail photo.");
-          return;
-        }
+      event.preventDefault();
+      if (!isfile) {
+        window.alert("Please select a thumbnail photo.");
+        return;
+      }
+      const quillContent = quillRef.current?.getEditor().getContents();
 
-        const file = fileInputRef.current.files[0];
-        const compressor = new ImageCompressor();
-        const compressedImage = await compressor.compress(file, {
-          maxWidth: 720,
-          maxHeight: 540,
-          quality: 0.8,
-          mimeType: 'image/webp',
-        });
+      let file = isfile;
 
-        const storage = getStorage();
-        const storagePath = 'thumbnails/' + file.name;
-        const storageRef = ref(storage, storagePath);
-        const uploadTask = uploadBytesResumable(storageRef, compressedImage);
+      const compressor = new ImageCompressor();
+      const compressedImage = await compressor.compress(file, {
+        maxWidth: 720,
+        maxHeight: 540,
+        quality: 0.8,
+        mimeType: 'image/webp',
+      });
 
-        uploadTask.on('state_changed', (snapshot) => {
-          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log('Upload is ' + progress + '% done');
-        },
+
+      const storage = getStorage();
+      var storagePath = 'thumbnail/' + file.name;
+
+      const storageRef = ref(storage, storagePath);
+      const uploadTask = uploadBytesResumable(storageRef, compressedImage);
+
+      uploadTask.on('state_changed', (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+      },
         (error) => {
           console.log(error);
         },
         () => {
           getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            console.log('File available at', downloadURL);
             let postdata = {
               title: formData.title,
               thumbnail: downloadURL,
-              content: quillRef.current?.getEditor().getContents(),
+              content: quillContent,
               type: formData.type,
               time: new Date().getTime(),
               authorName: auth.currentUser.displayName,
             };
-            saveposttodb('posts', postdata); // Save regular post to 'posts' collection
+            console.log(postdata);
+            saveposttodb(postdata);
           });
         });
-      }
 
       if (typeof window !== 'undefined') {
         window.alert("Form submitted");
@@ -165,14 +156,34 @@ function Page() {
       resetForm();
       router.push("/posts");
     } catch (error) {
-      console.error('Error submitting form:', error);
+      throw error;
+    }
+  };
+
+
+  const handleTestimonialExtraAction = async () => {
+    try {
+      const testimonialContent = value.trim();
+      if (testimonialContent.split(/\s+/).length > 70) {
+        window.alert("Testimonial content should not exceed 70 words.");
+        return;
+      }
+      if (!testimonialContent) {
+        window.alert("Please write your testimonial.");
+        return;
+      }
+      await saveTestimonialToDb(testimonialContent);
+      window.alert("Testimonial added successfully!");
+      resetForm();
+      router.push("/addnew");
+    } catch (error) {
+      console.error('Error adding testimonial:', error);
     }
   };
 
   return (
     <div className='container'>
       <h1>Upload Your Post<hr /></h1>
-      <form onSubmit={handleSubmit}>
         <FormControl isRequired>
           <FormLabel>Type of post</FormLabel>
           <Select
@@ -205,12 +216,13 @@ function Page() {
               required
             />
             <FormHelperText>{`${value.trim().split(/\s+/).length} / 70 words`}</FormHelperText>
+            <Button onClick={handleTestimonialExtraAction} colorScheme="blue" mt={4}>Add Testimonial</Button>
           </FormControl>
         ) : (
           <>
             <FormControl isRequired>
               <FormLabel>Title</FormLabel>
-              <Input variant='filled' placeholder='Enter the title' type='text' name='title' value={formData.title} onChange={onChangeHandler} required/>
+              <Input variant='filled' placeholder='Enter the title' type='text' name='title' value={formData.title} onChange={onChangeHandler} required />
               <FormHelperText></FormHelperText>
             </FormControl>
             <FormControl isRequired>
@@ -220,7 +232,7 @@ function Page() {
                 name='thumbnail'
                 accept='.png, .jpg, .jpeg'
                 onChange={handleImageAsFile}
-                ref={fileInputRef} 
+                ref={fileInputRef}
                 required
               />
               <FormHelperText></FormHelperText>
@@ -245,15 +257,13 @@ function Page() {
               }
               <FormHelperText></FormHelperText>
             </FormControl>
+            <FormControl style={{ paddingTop: '25px' }}>
+              <Button onClick={handleSubmit} colorScheme='yellow' type='submit' className='submit-button' mt={4}  >
+                Submit
+              </Button>
+            </FormControl>
           </>
         )}
-
-        <FormControl style={{ paddingTop: '25px' }}>
-          <Button colorScheme='yellow' type='submit' className='submit-button' mt={4}  >
-            Submit
-          </Button>
-        </FormControl>
-      </form>
     </div>
   );
 }
