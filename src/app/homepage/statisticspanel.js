b33a44e0476e85f9e958b99cf4332272e3f20ce4
sreejith <sreejithksgupta2255@gmail.com>
2024-07-14 17:23:02 +0530

    responsive statistics panel

--- src/app/homepage/statisticspanel.js
+++ src/app/homepage/statisticspanel.js
@@ -4,11 +4,11 @@ import { statistics } from './data';
 
 export default function Statisticspanel() {
     return (
-        <Flex justifyContent="center" gap={6} p={5} className='cardcontainer'>
+        <Flex justifyContent="center" gap={{ base: 6, md: 10 }} p={5} className='cardcontainer' flexWrap="wrap">
             {statistics.map((statistic, index) => (
                 <Circle
                     key={index}
-                    size="250px"
+                    size={{ base: "150px", md: "250px" }} // Responsive size based on screen width
                     bgGradient="linear(to-r, teal.500, green.500)"
                     boxShadow="xl"
                     _hover={{ transform: "scale(1.05)", transition: "0.3s" }}
@@ -16,14 +16,15 @@ export default function Statisticspanel() {
                     flexDirection="column"
                     justifyContent="center"
                     alignItems="center"
+                    textAlign="center"
+                    mb={{ base: 6, md: 0 }} // Margin bottom for spacing, 0 on medium screens
                 >
-                    <Heading fontSize="6xl" color="white" className='statsval'>
+                    <Heading fontSize={{ base: "4xl", md: "6xl" }} color="white" className='statsval'>
                         {statistic.text}
                     </Heading>
-                    <Text fontSize="3xl" color="whiteAlpha.800" className='statstitle'>
+                    <Text fontSize={{ base: "xl", md: "3xl" }} color="whiteAlpha.800" className='statstitle'>
                         {statistic.title}
                     </Text>
-
                 </Circle>
             ))}
         </Flex>
