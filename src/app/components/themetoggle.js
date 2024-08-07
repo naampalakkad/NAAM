3c4d864f5f3eea52aa4f8efe667d8c95c35e7711
sreejith <sreejithksgupta2255@gmail.com>
2024-08-08 00:07:26 +0530

    theme toggle fixed

--- src/app/components/themetoggle.js
+++ src/app/components/themetoggle.js
@@ -1,17 +1,22 @@
 "use client";
 
-import { HStack, Switch, useColorMode, useColorModeValue, Icon, Box } from "@chakra-ui/react";
+import { HStack, Switch, useColorMode, useColorModeValue, Box, useTheme } from "@chakra-ui/react";
 import { FaSun, FaMoon } from "react-icons/fa";
 
 export const ThemeToggleButton = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   const SwitchIcon = useColorModeValue(FaSun, FaMoon);
   const iconColor = useColorModeValue("yellow.500", "gray.500");
+  const { colors } = useTheme();
+
+  // Use colorMode to determine which icon to show
+  const themeIcon = colorMode === "dark" 
+    ? '/assets/moon.svg' 
+    : "/assets/sun.svg";
 
   return (
     <HStack alignItems="center" spacing={4}>
-      <Icon as={SwitchIcon} color={iconColor} w={6} h={6} />
-      <Box display="flex" alignItems="center">
+      <Box position="relative">
         <Switch
           isChecked={colorMode === "dark"}
           onChange={toggleColorMode}
@@ -19,6 +24,20 @@ export const ThemeToggleButton = () => {
           cursor="pointer"
           id="themeToggleButton"
           aria-label="Toggle Theme"
+          colorScheme="teal"
+          sx={{
+            '.chakra-switch__track': {
+              bg: useColorModeValue(colors.teal[600], colors.teal[600]),
+              _checked: {
+                bg: useColorModeValue(colors.teal[900], colors.teal[900]),
+              },
+            },
+            '.chakra-switch__thumb': {
+              '&:before': {
+                content: `url(${themeIcon})`,
+              },
+            },
+          }}
         />
       </Box>
     </HStack>
