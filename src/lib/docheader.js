fc26c3155a9a9fc4853b699f4bda3163c349998d
sreejith <sreejithksgupta2255@gmail.com>
2024-08-30 18:05:30 +0530

    cache working for some images

--- src/lib/docheader.js
+++ src/lib/docheader.js
@@ -4,6 +4,7 @@ function Header() {
     <head>
       <title>NAAM Alumni Association</title>
       <link rel="icon" href="/assets/logo.webp"/>
+      <meta charset="UTF-8">
       <meta name="color-scheme" content="light dark" />
       <meta name="description" content="Connect with fellow NAAM alumni, share experiences, and build lasting relationships." />
       <meta name="keywords" content="NAAM alumni, alumni network, connect, memories, careers" />
